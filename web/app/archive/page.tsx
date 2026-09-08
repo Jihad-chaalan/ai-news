3958142e82eb5051740f14cf519b70d1e197146d
Jihad Chaalan <chaalan2004@gmail.com>
2026-09-08 11:05:30 +0300

    update the archive caching

--- web/app/archive/page.tsx
+++ web/app/archive/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { getBriefingDates } from "@/lib/supabase-queries";
 
+export const revalidate = 60; 
+
+
 export default async function ArchivePage() {
   const dates = await getBriefingDates();
 
